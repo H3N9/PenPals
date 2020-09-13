8ef3450942c98a909f86e52cd72bd14d92cfde60
chanayus <chanayus1526@hotmail.com>
2020-09-13 12:22:17 +0700

    update 13/9/2020

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+
 import React from "react";
-import { StyleSheet, Text, View, ScrollView, StatusBar } from "react-native";
-import Home from "./src/pages/home";
+import Index from "./src/index";
 import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+
 export default function App() {
   return (
     <React.Fragment>
-      <StatusBar barStyle = "light-content"/>
+      <StatusBar style="light" />
       <LinearGradient colors={["#132031", "#022b6b"]} style={{ flex: 1 }}>
-        <Home />
+        <Index />
       </LinearGradient>
     </React.Fragment>
   );
--- src/pages/account.js
+++ src/pages/account.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import Navbar from "../components/navbar";
 import Homebar from "../components/homebar";
-import Content from "../components/content";
 import { LinearGradient } from "expo-linear-gradient";
 
-const Home = ({ navigation }) => {
+const Account = ({ navigation }) => {
   return (
     <LinearGradient colors={["#132031", "#022b6b"]} style={{ flex: 1 }}>
       <Homebar />
       <ScrollView style={{flex: 1}}>
-        <Content />
+        <Text>Hellow</Text>
       </ScrollView>
       <Navbar navigation={navigation} />
     </LinearGradient>
   );
 };
 
-export default Home;
+export default Account;
