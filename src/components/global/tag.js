32a50eb91bc53e475cc1c9b6d505c4429504d689
chanayus <chanayus1526@hotmail.com>
2020-10-04 16:59:05 +0700

    fix border tag for iOS

--- src/components/global/tag.js
+++ src/components/global/tag.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import {StyleSheet, Text, TouchableOpacity} from 'react-native'
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-const Tag = ({tagName}) => {
-    const tag = tagName
-    return (
-        <TouchableOpacity><Text style={styles.tagText}>{tag}</Text></TouchableOpacity>
-    )
-}
+const Tag = ({ tagName }) => {
+  const tag = tagName;
+  return (
+    <TouchableOpacity style={styles.tagButton}>
+      <Text style={styles.tagText}>{tag}</Text>
+    </TouchableOpacity>
+  );
+};
 const styles = StyleSheet.create({
-    tagText: {
-        marginRight: 10,
-        marginTop: 2,
-        padding: 2,
-        borderRadius: 5,
-        backgroundColor: "#FF5350",
-        fontSize: 12,
-      },
-})
-export default Tag
+  tagText: {
+    fontSize: 12,
+  },
+  tagButton: {
+    marginRight: 10,
+    marginTop: 2,
+    padding: 2,
+    backgroundColor: "#FF5350",
+    borderRadius: 5,
+  },
+});
+export default Tag;
