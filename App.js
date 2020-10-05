ace8feed3d61659b2581e7b4d14a0746b0abc668
chanayus <chanayus1526@hotmail.com>
2020-10-05 21:57:18 +0700

    ChatRoom

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Index from "./src/index";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
-import Chat from './src/pages/chatRoom'
+import Chat from "./src/pages/chatRoom";
 
 export default function App() {
   return (
     <React.Fragment>
       <StatusBar style="light" />
-        <Index />
+      <Index />
     </React.Fragment>
   );
 }
