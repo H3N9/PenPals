f0e3029c8b76b8281bc6cd6c19ba173c65c29735
t <38640424+poohcid@users.noreply.github.com>
2020-10-13 11:49:13 +0700

    fetch test

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Index from "./src/index";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import Chat from "./src/pages/chatRoom";
 
 export default function App() {
+
   return (
     <React.Fragment>
       <StatusBar style="light" />
