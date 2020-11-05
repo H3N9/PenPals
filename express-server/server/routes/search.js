c80a741b63c60c0c70adb485accb2ca3614ca2b5
t <38640424+poohcid@users.noreply.github.com>
2020-11-05 20:49:24 +0700

    search tag

--- express-server/server/routes/search.js
+++ express-server/server/routes/search.js
@@ -6,11 +6,9 @@ const getProfile = require('../tools/getProfile')
 const filterUserByTag = require('../tools/filterUserByTag')
 const createQueryProfile = require('../tools/createQueryProfile')
 
-const { Tag, Profile } = db
+const { Tag, Profile, FavTag, Category } = db
 
 router.get('/user', async (req, res) =>{
-  // const tagIdList = req.body.tag
-  //let queryInId = await filterUserByTag(req.body.tag)
   let query = await createQueryProfile(req)
   const profile = await getProfile(query)
   res.json(profile)
@@ -20,6 +18,36 @@ router.get('/user/:id', async (req, res) =>{
   const id = req.params.id
   const profile = await getProfile({profileQuery: {userId: id}, otherQuery: {user: req.user}})
   res.json(...profile)
+})
+
+router.get('/hobb-tag', async (req, res) =>{
+  const nameQuery = req.body.name
+  const hobbTags = await Tag.findAll({
+    attributes: ["id", "name", "type"], 
+    where:{name: {[Op.like]:"%"+nameQuery+"%"}, type:"hobbies"}
+  })
+  res.json(hobbTags)
+})
+
+router.get('/fav-tag', async (req, res) =>{
+  const nameQuery = req.body.name
+  const favTags = await Tag.findAll({
+    attributes: ["id", "name", "type"], 
+    where:{name: {[Op.like]:"%"+nameQuery+"%"}, type:"favorites"}, include: [
+      {model: FavTag, as: "favTag", include: [
+        {model: Category, as: "category", attributes: ["id", "name"]}
+      ]}
+    ]
+  })
+
+  const favTagResponse = favTags.map((item1) =>{
+    const category = item1.dataValues.favTag.category.name
+    const returnData = item1.dataValues
+    delete returnData["favTag"]
+    return {...returnData, category}
+  })
+
+  res.json(favTagResponse)
 })
 
 module.exports = router;
