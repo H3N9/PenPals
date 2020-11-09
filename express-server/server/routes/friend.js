90603cfb419653f7a200a8bb76560c2eb20d0046
t <38640424+poohcid@users.noreply.github.com>
2020-11-10 00:32:25 +0700

    add addFreind

--- express-server/server/routes/friend.js
+++ express-server/server/routes/friend.js
@@ -23,6 +23,53 @@ router.get('/:id', async (req, res) =>{
     res.json(users)
 })
 
+router.put('/set-relationship', async (req, res) =>{
+    const user = req.user
+    const friendId = req.body.friendId
+    const myProfile = req.user.profile
+    const parties = await Profile.findByPk(friendId)
+    let relationshipState = "not friend"
+
+    let list = await getRelationship({ id: myProfile.id, type: "" })
+    console.log(list)
+    const myFriend = list.friends.map((item1) =>{
+        if (item1.id === friendId)
+            relationshipState = item1.relationshipState
+        return item1.id
+    })
+
+    list = await getRelationship({ id: friendId, type: "" })
+    const partiesFriend = list.friends.map((item1) => item1.id)
+
+    if (relationshipState === "not friend"){
+        await myProfile.setFriend([...myFriend, friendId])
+    }
+    else if (relationshipState === "friend request"){
+        await myProfile.setFriend(myFriend)
+    }
+    else if (relationshipState === "request sent"){
+        let friendFilter = myFriend.filter((item1) =>{
+            return item1 !== friendId
+        })
+        await myProfile.setFriend(friendFilter)
+    }
+    else if (relationshipState === "friend"){
+        let friendFilter = myFriend.filter((item1) =>{
+            return item1 !== friendId
+        })
+        await myProfile.setFriend(friendFilter)
+        friendFilter = partiesFriend.filter((item1) =>{
+            return item1 !==  myProfile.id
+        })
+        await parties.setFriend(friendFilter)
+    }
+
+
+    const response = await getProfile({ profileQuery: {id: friendId }, otherQuery: { user } })
+    
+    res.json(...response)
+})
+
 router.get('/count/:id', async (req, res) =>{
     const id = req.params.id
 
